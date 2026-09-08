b4c92e90728c35524f4108b59bbbf107478b492f
code4life <code4lifestyle@gmail.com>
2026-09-08 19:20:38 +0500

    layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-1 items-center justify-center px-4 py-12">
       <Signup />
+      <h2 className="text-center">this is home page</h2>
     </main>
   );
 }
